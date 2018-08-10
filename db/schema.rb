f398d54557baa02ac71a9f215f594b816d817fa9
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-08-10 14:53:20 +0200

    Added Paperclip and image upload feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_10_105018) do
+ActiveRecord::Schema.define(version: 2018_08_10_112531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2018_08_10_105018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
